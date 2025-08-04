eab2cdac6da945423bb31844c1c424c4fb7745b3
Lufthi.as <lufthi73.com@gmail.com>
2025-08-04 17:46:53 +0700

    airbnb_dashboard.py

--- airbnb_dashboard.py
+++ airbnb_dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 from datetime import datetime
 
@@ -385,3 +385,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
